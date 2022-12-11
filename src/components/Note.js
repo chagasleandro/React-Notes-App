b522470160ef39d04ad79b8c5439288c0f93b4b6
chagasleandro <leandro_srs20@yahoo.com.br>
2022-12-11 20:59:55 -0300

    add index.css

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,8 +6,7 @@ const Note = () => {
       <span>Hello! This is our first note! Chagas!</span>
       <div className="note-footer">
         <small>11/12/2022</small>
-        <MdDeleteForever className="delete-icons" size='1.3em' />
-        
+        <MdDeleteForever className="delete-icons" size='1.3em' /> 
       </div>
     </div>
   );
